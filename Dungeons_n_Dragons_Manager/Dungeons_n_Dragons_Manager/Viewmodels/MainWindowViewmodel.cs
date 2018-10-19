87c1bd7a949985f9a75ab0cdb6496b3d0606dfcb
Bunty Dey <s156d039@ku.edu>
2018-10-18 19:37:16 -0500

    dice roller

--- Dungeons_n_Dragons_Manager/Dungeons_n_Dragons_Manager/Viewmodels/MainWindowViewmodel.cs
+++ Dungeons_n_Dragons_Manager/Dungeons_n_Dragons_Manager/Viewmodels/MainWindowViewmodel.cs
@@ -20,6 +20,16 @@ namespace Dungeons_n_Dragons_Manager.Viewmodels
             set { }
         }
 
+        private DiceRollTabViewmodel m_diceRollTabViewModel = new DiceRollTabViewmodel();
+        public DiceRollTabViewmodel DiceRollTabViewmodel
+        {
+            get
+            {
+                return m_diceRollTabViewModel;
+            }
+        }
+
+
         #endregion
     }
 }
