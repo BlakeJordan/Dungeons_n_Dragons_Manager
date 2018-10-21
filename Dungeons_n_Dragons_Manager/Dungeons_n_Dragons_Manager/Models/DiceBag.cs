fde262532b866b54dbf021d5b193668dea078b55
Bunty Dey <s156d039@ku.edu>
2018-10-21 18:58:43 -0500

    added documenation to DiceBag class

--- Dungeons_n_Dragons_Manager/Dungeons_n_Dragons_Manager/Models/DiceBag.cs
+++ Dungeons_n_Dragons_Manager/Dungeons_n_Dragons_Manager/Models/DiceBag.cs
@@ -4,17 +4,36 @@ using System.ComponentModel;
 
 namespace Dungeons_n_Dragons_Manager.Models
 {
+    /// <summary>
+    /// DiceBag class containing rolling functions dice enum.
+    /// </summary>
     internal class DiceBag : INotifyPropertyChanged
     {
+        /// <summary>
+        /// private variable which will hold a random number for each roll
+        /// </summary>
         private Random rng;
-
+        /// <summary>
+        /// contructor for the DiceBag class
+        /// @Pre: random number variable has been instantiated
+        /// @Post: rng is set to a new Random
+        /// </summary>
         public DiceBag()
         {
             rng = new Random();
         }
-
+        /// <summary>
+        /// private facing enum holding each type of dice set to their numerical max
+        /// </summary>
         public enum Dice : uint { D4 = 4, D6 = 6, D8 = 8, D10 = 10, D12 = 12, D20 = 20, D100 = 100 };
-
+        /// <summary>
+        /// rolls a single dice the number of times specified by the user
+        /// @Pre: Roll has been clicked and a number exists in the textbox next to the specified dice
+        /// @Post: returns a list of strings containing the rolls for the specified dice
+        /// </summary>
+        /// <param name="dice"></param>
+        /// <param name="times"></param>
+        /// <returns></returns>
         private List<string> Roll(int dice, int times)
         {
             List<string> rolls = new List<string>();
