2419cf0f575a289df94c609941f09b3682628eb8
wildman06 <austinwildman7@gmail.com>
2018-11-08 18:42:28 -0700

    partially fixed dropdowns for create monster.

--- Dungeons_n_Dragons_Manager/Dungeons_n_Dragons_Manager/Viewmodels/CreateMonsterWindowViewmodel.cs
+++ Dungeons_n_Dragons_Manager/Dungeons_n_Dragons_Manager/Viewmodels/CreateMonsterWindowViewmodel.cs
@@ -91,6 +91,43 @@ namespace Dungeons_n_Dragons_Manager.Viewmodels
         public bool IsUrban { get; set; }
 
         /// <summary>
+        /// The character's options for each skill's level
+        /// </summary>
+        public List<string> SkillValues { get; set; }
+        /// <summary>
+        /// Command binded to proficiency checkboxes which calls proficiencyCheck
+        /// </summary>
+        private ICommand m_EnvironmentCheck;
+
+        public bool IsEnvironmentChecked { get; set; }
+
+        /// <summary>
+        /// Public facing accessor for m_ProficiencyCheck
+        /// </summary>
+        public ICommand EnvironmentCheck
+        {
+            get
+            {
+                return m_EnvironmentCheck ?? (m_EnvironmentCheck = new CommandHandler(() => SetEnvironment(IsEnvironmentChecked), true));
+            }
+        }
+
+        /// <summary>
+        /// Checks if a environment can be added, then adds the environment to the monster's environment list
+        /// </summary>
+        public void SetEnvironment(bool canSetEnvironemnt)
+        {
+            if (IsEnvironmentChecked == true)
+            {
+
+            }
+            else
+            {
+
+            }
+        }
+
+        /// <summary>
         /// Populates the dropdown menus for the armor type options
         ///
         /// Pre: None
